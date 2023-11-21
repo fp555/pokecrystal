7d953cf357ae5e375ac5d7c4596ca87c10aa8f74
Fabio P <7722100+fp555@users.noreply.github.com>
2023-11-21 20:46:31 +0100

    Counter and Mirror Coat still work if the opponent uses an item

--- engine/battle/move_effects/counter.asm
+++ engine/battle/move_effects/counter.asm
@@ -5,41 +5,32 @@ BattleCommand_Counter:
 	call GetBattleVar
 	and a
 	ret z
-
 	ld b, a
 	callfar GetMoveEffect
 	ld a, b
 	cp EFFECT_COUNTER
 	ret z
-
 	call BattleCommand_ResetTypeMatchup
 	ld a, [wTypeMatchup]
 	and a
 	ret z
-
 	call CheckOpponentWentFirst
 	ret z
-
 	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
 	call GetBattleVar
 	dec a
 	ld de, wStringBuffer1
 	call GetMoveData
-
 	ld a, [wStringBuffer1 + MOVE_POWER]
 	and a
 	ret z
-
 	ld a, [wStringBuffer1 + MOVE_TYPE]
 	cp SPECIAL
 	ret nc
-
-; BUG: Counter and Mirror Coat still work if the opponent uses an item (see docs/bugs_and_glitches.md)
 	ld hl, wCurDamage
 	ld a, [hli]
 	or [hl]
-	ret z
-
+	jr z, .failed
 	ld a, [hl]
 	add a
 	ld [hld], a
@@ -51,7 +42,11 @@ BattleCommand_Counter:
 	ld [hli], a
 	ld [hl], a
 .capped
-
 	xor a
 	ld [wAttackMissed], a
+	ret
+.failed
+	ld a, 1
+	ld [wEffectFailed], a
+	and a
 	ret

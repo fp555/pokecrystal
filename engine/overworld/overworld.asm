9e025714b4b832b9c29743926b9a15c77b0f658d
Fabio P. <7722100+fp555@users.noreply.github.com>
2024-05-25 12:39:46 +0200

    Improve the outdoor sprite system

--- engine/overworld/overworld.asm
+++ engine/overworld/overworld.asm
@@ -122,15 +122,12 @@ AddOutdoorSprites:
 	ld a, [hli]
 	ld h, [hl]
 	ld l, a
-	ld c, MAX_OUTDOOR_SPRITES
 .loop
-	push bc
 	ld a, [hli]
+	and a
+	ret z
 	call AddSpriteGFX
-	pop bc
-	dec c
-	jr nz, .loop
-	ret
+	jr .loop
 
 LoadUsedSpritesGFX:
 	ld a, MAPCALLBACK_SPRITES
@@ -281,7 +278,6 @@ _GetSpritePalette::
 
 LoadAndSortSprites:
 	call LoadSpriteGFX
-	call SortUsedSprites
 	call ArrangeUsedSprites
 	ret
 
@@ -332,7 +328,6 @@ LoadSpriteGFX:
 	ld [hli], a
 	dec b
 	jr nz, .loop
-
 .done
 	ret
 .LoadSprite:
@@ -342,63 +337,6 @@ LoadSpriteGFX:
 	ld a, l
 	ret
 
-SortUsedSprites:
-; Bubble-sort sprites by type.
-; Run backwards through wUsedSprites to find the last one.
-	ld c, SPRITE_GFX_LIST_CAPACITY
-	ld de, wUsedSprites + (SPRITE_GFX_LIST_CAPACITY - 1) * 2
-.FindLastSprite:
-	ld a, [de]
-	and a
-	jr nz, .FoundLastSprite
-	dec de
-	dec de
-	dec c
-	jr nz, .FindLastSprite
-.FoundLastSprite:
-	dec c
-	jr z, .quit
-	; If the length of the current sprite is higher than a later one, swap them.
-	inc de
-	ld hl, wUsedSprites + 1
-.CheckSprite:
-	push bc
-	push de
-	push hl
-.CheckFollowing:
-	ld a, [de]
-	cp [hl]
-	jr nc, .loop
-	; Swap the two sprites.
-	ld b, a
-	ld a, [hl]
-	ld [hl], b
-	ld [de], a
-	dec de
-	dec hl
-	ld a, [de]
-	ld b, a
-	ld a, [hl]
-	ld [hl], b
-	ld [de], a
-	inc de
-	inc hl
-	; Keep doing this until everything's in order.
-.loop
-	dec de
-	dec de
-	dec c
-	jr nz, .CheckFollowing
-	pop hl
-	inc hl
-	inc hl
-	pop de
-	pop bc
-	dec c
-	jr nz, .CheckSprite
-.quit
-	ret
-
 ArrangeUsedSprites:
 ; Get the length of each sprite and space them out in VRAM.
 ; Crystal introduces a second table in VRAM bank 0.
@@ -406,7 +344,7 @@ ArrangeUsedSprites:
 	ld c, SPRITE_GFX_LIST_CAPACITY
 	ld b, 0
 .FirstTableLength:
-; Keep going until the end of the list.
+	; Keep going until the end of the list.
 	ld a, [hli]
 	and a
 	jr z, .quit
@@ -425,11 +363,11 @@ ArrangeUsedSprites:
 	dec c
 	jr nz, .FirstTableLength
 .SecondTable:
-; The second tile table starts at tile $80.
+	; The second tile table starts at tile $80.
 	ld b, $80
 	dec hl
 .SecondTableLength:
-; Keep going until the end of the list.
+	; Keep going until the end of the list.
 	ld a, [hli]
 	and a
 	jr z, .quit

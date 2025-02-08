69f121e9c1580b2a2bd7f541f8c3607a3b9d9415
Fabio P. <7722100+fp555@users.noreply.github.com>
2025-02-08 12:38:15 +0100

    Remove leftover warp events in Burned Tower 1F-B1F

--- maps/BurnedTower1F.asm
+++ maps/BurnedTower1F.asm
@@ -71,7 +71,6 @@ BurnedTowerRivalBattleScript:
 	dontrestartmapmusic
 	reloadmapafterbattle
 	sjump .returnfrombattle
-
 .totodile
 	winlosstext BurnedTowerRival_WinText, BurnedTowerRival_LossText
 	setlasttalked BURNEDTOWER1F_RIVAL
@@ -80,7 +79,6 @@ BurnedTowerRivalBattleScript:
 	dontrestartmapmusic
 	reloadmapafterbattle
 	sjump .returnfrombattle
-
 .chikorita
 	winlosstext BurnedTowerRival_WinText, BurnedTowerRival_LossText
 	setlasttalked BURNEDTOWER1F_RIVAL
@@ -89,7 +87,6 @@ BurnedTowerRivalBattleScript:
 	dontrestartmapmusic
 	reloadmapafterbattle
 	sjump .returnfrombattle
-
 .returnfrombattle
 	playmusic MUSIC_RIVAL_AFTER
 	opentext
@@ -282,17 +279,7 @@ BurnedTower1F_MapEvents:
 	warp_event  9, 15, ECRUTEAK_CITY, 13
 	warp_event 10, 15, ECRUTEAK_CITY, 13
 	warp_event 10,  9, BURNED_TOWER_B1F, 1
-	warp_event  5,  5, BURNED_TOWER_B1F, 1 ; inaccessible, left over from G/S
-	warp_event  5,  6, BURNED_TOWER_B1F, 1 ; inaccessible, left over from G/S
-	warp_event  4,  6, BURNED_TOWER_B1F, 1 ; inaccessible, left over from G/S
-	warp_event 15,  4, BURNED_TOWER_B1F, 2 ; inaccessible, left over from G/S
-	warp_event 15,  5, BURNED_TOWER_B1F, 2 ; inaccessible, left over from G/S
-	warp_event 10,  7, BURNED_TOWER_B1F, 3 ; inaccessible, left over from G/S
-	warp_event  5, 14, BURNED_TOWER_B1F, 4 ; inaccessible, left over from G/S
-	warp_event  4, 14, BURNED_TOWER_B1F, 4 ; inaccessible, left over from G/S
-	warp_event 14, 14, BURNED_TOWER_B1F, 5 ; inaccessible, left over from G/S
-	warp_event 15, 14, BURNED_TOWER_B1F, 5 ; inaccessible, left over from G/S
-	warp_event  7, 15, BURNED_TOWER_B1F, 6 ; inaccessible, left over from G/S
+	warp_event  7, 15, BURNED_TOWER_B1F, 2
 
 	def_coord_events
 	coord_event 11,  9, SCENE_BURNEDTOWER1F_RIVAL_BATTLE, BurnedTowerRivalBattleScript

eba801400dd78dc57db9eed21c88d44d1d01d5d5
Fabio P. <7722100+fp555@users.noreply.github.com>
2026-01-17 15:38:10 +0100

    Automatically reuse Repels

--- engine/events/repel.asm
+++ engine/events/repel.asm
@@ -1,10 +1,21 @@
 RepelWoreOffScript::
 	opentext
+	readmem wRepelType
+	getitemname STRING_BUFFER_3, USE_SCRIPT_VAR
 	writetext .RepelWoreOffText
 	waitbutton
+	checkitem USE_SCRIPT_VAR ; one of REPEL, SUPER_REPEL, MAX_REPEL
+	iffalse .done
+	writetext .RepelUseAnotherText
+	yesorno
+	iffalse .done
+	callasm DoItemEffect
+.done
 	closetext
 	end
-
 .RepelWoreOffText:
 	text_far _RepelWoreOffText
+	text_end
+.RepelUseAnotherText:
+	text_far _RepelUseAnotherText
 	text_end

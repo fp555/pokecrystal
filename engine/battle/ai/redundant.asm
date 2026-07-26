331d064afca45c68ad19522d4136ade2a77fc072
Fabio P. <7722100+fp555@users.noreply.github.com>
2026-07-26 11:07:38 +0200

    AI does not discourage Nightmare if the player has any status condition

--- engine/battle/ai/redundant.asm
+++ engine/battle/ai/redundant.asm
@@ -104,9 +104,8 @@ AI_Redundant:
 	bit SUBSTATUS_CANT_RUN, a
 	ret
 .Nightmare:
-; BUG: AI does not discourage Nightmare if the player has any status condition (see docs/bugs_and_glitches.md)
 	ld a, [wBattleMonStatus]
-	and a
+	and SLP_MASK
 	jr z, .Redundant
 	ld a, [wPlayerSubStatus1]
 	bit SUBSTATUS_NIGHTMARE, a
